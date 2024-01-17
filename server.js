fcc90a014f5ef4ccb629fbc7af119eb0eb2e097a
reserver7 <reserver7@gmail.com>
2024-01-17 16:22:16 +0900

    apollo 적용해보기

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const { ApolloServer } = require("@apollo/server");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
+// const { graphqlHTTP } = require("express-graphql");
 const path = require("path");
-const app = express();
 const port = 4000;
+const cors = require("cors");
+const { json } = require("body-parser");
+const { expressMiddleware } = require("@apollo/server/express4");
 
 const loadedFiles = loadFilesSync("**/*", {
   extensions: ["graphql"],
@@ -14,27 +17,50 @@ const loadedResolvers = loadFilesSync(
   path.join(__dirname, "**/*.resolvers.js")
 );
 
-const schema = makeExecutableSchema({
-  typeDefs: loadedFiles,
-  resolvers: loadedResolvers,
-});
+async function startApolloServer() {
+  const app = express();
+
+  const schema = makeExecutableSchema({
+    typeDefs: loadedFiles,
+    resolvers: loadedResolvers,
+  });
+
+  const server = new ApolloServer({ schema });
+
+  await server.start();
+
+  app.use(
+    "/graphql",
+    cors(),
+    json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    })
+  );
+
+  app.listen(port, () => {
+    console.log(`Running a GraphQL API server...`);
+  });
+}
+
+startApolloServer();
 
 // const root = {
 //   posts: require("./posts/posts.model"),
 //   comments: require("./comments/comments.model"),
 // };
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    // rootValue: root,
-    graphiql: true,
-  })
-);
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema: schema,
+//     // rootValue: root,
+//     graphiql: true,
+//   })
+// );
 
-app.listen(port, () => {
-  console.log(
-    `Running a GraphQL API server at http://localhost:${port}/graphql`
-  );
-});
+// app.listen(port, () => {
+//   console.log(
+//     `Running a GraphQL API server at http://localhost:${port}/graphql`
+//   );
+// });
